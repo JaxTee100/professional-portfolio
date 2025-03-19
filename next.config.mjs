542a79ad2ae897a5437b3d9aa2828f8efaed9743
Udeh Tochukwu Tobias <jaxj9449@gmail.com>
2025-03-19 10:04:15 +0100

    adjusted the nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: { appDir: true }, 
+  output: 'standalone',
+};
 
 export default nextConfig;
